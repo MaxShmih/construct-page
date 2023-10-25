5403216e24d2495cb94d1f6984c38e997eccd2f6
Max <max@MacBook-Pro-Max.local>
2023-10-25 16:29:51 +0300

    added scroll to second block on hero page, deleted some unnecessary elements, modify scroll block

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ const containerWidth = container.offsetWidth;
 const lineBackdown = (containerWidth - screenWidth);
 const lineRule = styleSheet.insertRule('.header::after { left: ' + lineBackdown / 2 + 'px; }', styleSheet.cssRules.length);
 
+const header = document.querySelector('.header');
+const scrollBlock = document.querySelector('.hero__scroll');
+
+scrollBlock.addEventListener('click', function(e) {
+		header.scrollIntoView({
+			behavior: 'smooth'
+	})
+})
+
+const scrollSpan = document.querySelectorAll('.letter').forEach(el => {
+	el.addEventListener('mouseenter', function(){
+		el.classList.add('puls');
+		setTimeout(() => {
+			el.classList.remove('puls')
+		}, 1000)
+		})
+
+
+	})
