607850c89550812b8829e16649f3bd9833640ce4
Max <max@MacBook-Pro-Max.local>
2023-10-28 01:21:58 +0300

    fixed HR and cards

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-const screenWidth = document.body.clientWidth;
-const styleSheet = document.styleSheets[0];
-const widthRule = styleSheet.insertRule('.header::after { width: ' + screenWidth + 'px; }', styleSheet.cssRules.length);
-const container = document.querySelector('.container');
-const containerWidth = container.offsetWidth;
-const lineBackdown = (containerWidth - screenWidth);
-const lineRule = styleSheet.insertRule('.header::after { left: ' + lineBackdown / 2 + 'px; }', styleSheet.cssRules.length);
-
 const header = document.querySelector('.header');
 const scrollBlock = document.querySelector('.hero__scroll');
 
@@ -17,9 +9,9 @@ scrollBlock.addEventListener('click', function(e) {
 
 const scrollSpan = document.querySelectorAll('.letter').forEach(el => {
 	el.addEventListener('mouseenter', function(){
-		el.classList.add('puls');
+		el.classList.add('jump');
 		setTimeout(() => {
-			el.classList.remove('puls')
+			el.classList.remove('jump')
 		}, 1000)
 		})
 
