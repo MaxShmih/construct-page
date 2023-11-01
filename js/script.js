71f298b3576a39081c5ec49c5de163b68ef9101b
Max <max@MacBook-Pro-Max.local>
2023-11-01 14:27:53 +0200

    50% mobile and without slider

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 const header = document.querySelector('.header');
 const scrollBlock = document.querySelector('.hero__scroll');
+const burger = document.querySelector('.menu__btn');
+const burgerMenu = document.querySelector('.burger__wrapp')
 
-scrollBlock.addEventListener('click', function(e) {
-		header.scrollIntoView({
-			behavior: 'smooth'
+burger.addEventListener('click', function () {
+	burger.classList.toggle('active');
+	burgerMenu.classList.toggle('active');
+	burgerMenu.addEventListener('click', function() {
+		burgerMenu.classList.remove('active')
+		burger.classList.remove('active')
+	})
+})
+
+scrollBlock.addEventListener('click', function () {
+	header.scrollIntoView({
+		behavior: 'smooth'
 	})
 })
 
 const scrollSpan = document.querySelectorAll('.letter').forEach(el => {
-	el.addEventListener('mouseenter', function(){
+	el.addEventListener('mouseenter', function () {
 		el.classList.add('jump');
 		setTimeout(() => {
 			el.classList.remove('jump')
 		}, 1000)
-		})
-
-
 	})
+
+
+})
