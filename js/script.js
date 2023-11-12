0b41e0f1147e12efba35b36c1f03ea64833c3a02
Max <max@MacBook-Pro-Max.local>
2023-11-12 21:14:45 +0200

    done mobile version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
+const body = document.querySelector('.body');
 const header = document.querySelector('.header');
 const scrollBlock = document.querySelector('.hero__scroll');
 const burger = document.querySelector('.menu__btn');
-const burgerMenu = document.querySelector('.burger__wrapp')
+const burgerMenu = document.querySelector('.burger__wrapp');
+
+const swiper = new Swiper('.swiper', {
+	loop: true,
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
 
 burger.addEventListener('click', function () {
+	body.classList.toggle('active')
 	burger.classList.toggle('active');
 	burgerMenu.classList.toggle('active');
-	burgerMenu.addEventListener('click', function() {
-		burgerMenu.classList.remove('active')
-		burger.classList.remove('active')
+	burgerMenu.addEventListener('click', function () {
+		burgerMenu.classList.remove('active');
+		burger.classList.remove('active');
+		body.classList.remove('active');
 	})
 })
 
